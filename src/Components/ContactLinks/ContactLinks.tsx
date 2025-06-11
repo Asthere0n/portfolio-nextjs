8da6a8391282666844f86942fe134ac4d5ba0b27
Cristian Sanchez <cristiansanchezsonido@gmail.com>
2025-06-11 20:20:46 +0100

    Stylized the Hero section, added icons to every CTA button

--- src/Components/ContactLinks/ContactLinks.tsx
+++ src/Components/ContactLinks/ContactLinks.tsx
@@ -1,14 +1,30 @@
 import React from 'react'
+import GitHub from './GitHub'
+import  Link  from 'next/link'
+import LinkedIn from './LinkedIn'
+import DownloadCV from './DownloadCV'
+import Location from './Location'
+
 
 export default function ContactLinks() {
     return (
-        <div className='w-full flex justify-center items-center p-5'>
-            <ul className='w-full flex justify-between '>
-                <li className='p-4 rounded-3xl border-2 border-white'>Github</li>
-                <li className='p-4 rounded-3xl border-2 border-white'>LinkedIn</li>
-                <li className='p-4 rounded-3xl border-2 border-white'>Download CV</li>
-                <li className='p-4 rounded-3xl border-2 border-white'>Stirling</li>
-            </ul>
+        <div className='w-full flex justify-around items-center'>
+            <Link href='https://github.com/Asthere0n'
+                className='border-4 border-white rounded-2xl p-3 h-24 w-32 flex flex-col justify-between items-center'>
+                    <GitHub/><p className='text-xl'>GitHub</p>
+                </Link>
+            <Link href='https://github.com/Asthere0n'
+                className='border-4 border-white rounded-2xl p-3 h-24 w-32 flex flex-col justify-between items-center'>
+                    <LinkedIn/><p className='text-xl'>LinkedIn</p>
+                </Link>
+            <Link href='https://github.com/Asthere0n'
+                className='border-4 border-white rounded-2xl p-3 h-24 w-32 flex flex-col justify-between items-center'>
+                    <DownloadCV/><p className='text-xl'>CV</p>
+                </Link>
+            <Link href='https://github.com/Asthere0n'
+                className='border-4 border-white rounded-2xl p-3 h-24 w-32 flex flex-col justify-between items-center'>
+                    <Location/><p className='text-xl'>Stirling</p>
+                </Link>
         </div>
     )
 }
