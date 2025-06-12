edf978f97be30f7c6acecf5431e70a02a4dc2c41
Cristian Sanchez <cristiansanchezsonido@gmail.com>
2025-06-12 19:02:59 +0100

    Started the styles on the Stack component. Other technologies missing

---
+++ src/Components/Stack/MERNLetter/MERNLetter.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export interface Props {
+  name: string,
+  color: string,
+  icon: any,
+  delay: number,
+  subtitle: string
+}
+
+export default function MERNLetter(props: Props) {
+  const name = props.name
+  const initial = name.slice(0, 1)
+  const rest = name.slice(1)
+  const color = props.color
+
+  return (
+    <div className='text-white flex items-center'>
+      <div className={`border-4 border-white bg-${color}-400 text-5xl w-20 h-20 flex justify-center items-center p-4 rounded-full m-4`}>
+      {props.icon}
+      </div>
+      <div>
+      <h4 className='text-4xl'><span className={`text-4xl text-${color}-400`}>{initial}</span>{rest}</h4>
+        <p className='text-xl text-slate-400'>{props.subtitle}</p>
+      </div>
+    </div>
+  )
+}
--- src/Components/Stack/Stack.tsx
+++ src/Components/Stack/Stack.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
+import MERNLetter from './MERNLetter/MERNLetter'
+import MongoDB from './MERNLetter/Logos/MongoDB'
+import Expressjs from './MERNLetter/Logos/Expressjs'
+import ReactJS from './MERNLetter/Logos/ReactJS'
+import Nodejs from './MERNLetter/Logos/Nodejs'
 
 export default function Stack() {
     return (
-        <>
-            <div>
-                <h2>Prefered Stack</h2>
-                <ul>
-                    <li>MongoDB</li>
-                    <li>ExpressJS</li>
-                    <li>React</li>
-                    <li>NodeJS</li>
-                </ul>
+        <div className='w-full min-h-[100dvh] flex justify-around items-center '>
+            <div className='m-8 flex-1'>
+                <h2 className='text-5xl mb-4'>But what's <span className='text-yellow-400'>MERN</span>?</h2>
+                <p className='text-2xl text-slate-400'>Mern is a set of tools that allow us to build complete application from top to bottom without ever leaving JavaScript.</p>
+                <MERNLetter name='MongoDB' delay={0} icon={<MongoDB />} color='green' subtitle='NoSQL Database' />
+                <MERNLetter name='Express' delay={10} icon={<Expressjs />} color='yellow' subtitle='Back end Framework to build RESTful APIs' />
+                <MERNLetter name='React' delay={10} color='cyan' icon={<ReactJS />} subtitle='Front end Framework to build user interfaces' />
+                <MERNLetter name='NodeJs' delay={30} color='lime' icon={<Nodejs/>} subtitle='JavaScript Runtime environment' />
             </div>
-            <div>
-                <h3>Other <span>Technologies</span> I'm familiar with</h3>
+            <div className='m-8'>
+                <h3>Other <span>Technologies</span> I work with</h3>
                 <ul>
                     <li>JavaScript</li>
                     <li>Tailwind</li>
@@ -26,6 +30,6 @@ export default function Stack() {
                     <li>Miro</li>
                 </ul>
             </div>
-        </>
+        </div>
     )
 }
