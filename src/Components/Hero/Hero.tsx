eaa2af1778e9fc4146d63c1beb35e9fbb12cbbc9
Cristian Sanchez <cristiansanchezsonido@gmail.com>
2025-06-13 12:38:04 +0100

    Refactor all icons into a new shared folder out of the component. Added languages spoken and Availability to work to Hero section.

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Image from 'next/image'
 import ContactLinks from '../ContactLinks/ContactLinks'
+import SpainFlag from '../../../public/icons/SpainFlag'
+import UKFlag from '../../../public/icons/UKFlag'
 
 export default function Hero() {
   return (
@@ -8,10 +10,15 @@ export default function Hero() {
       <Image src="/Portrait.webp" height={500} width={500} alt='Portrait of a beatiful Spanish full-stack developer' className='rounded-full h-90 w-90 m-15' />
       <div className='flex flex-col gap-8 items-start p-12'>
         <h2 className='text-5xl p-5'>Hi! I am <span className='text-yellow-400'>Cristian Sanchez</span></h2>
+        <div className='flex gap-5 px-5 items-center'>
+          <SpainFlag className='w-12' />
+          <UKFlag className='w-12' />
+          <h3 className='text-slate-400 text-3xl'>Bylingual | <span className='text-xl text-green-400 p-3 border-2 border-green-400 rounded-lg '>LOOKING FOR WORK</span></h3>
+        </div>
         <p className='text-lg p-5 text-justify'>
           A former hospitality manager turned <span className='text-yellow-400'>Full-Stack Developer</span>. I use to lead teams in fast-paced environments - now I build responsive, user-focused web apps and tools using the MERN stack. Same chaos, just less dirty laundry.
         </p>
-      <ContactLinks />
+        <ContactLinks />
       </div>
     </div>
   )
--- src/Components/Stack/Stack.tsx
+++ src/Components/Stack/Stack.tsx
@@ -1,25 +1,25 @@
 import React from 'react'
 import MERNLetter from './MERNLetter/MERNLetter'
-import MongoDB from './MERNLetter/Logos/MongoDB'
-import Expressjs from './MERNLetter/Logos/Expressjs'
-import ReactJS from './MERNLetter/Logos/ReactJS'
-import Nodejs from './MERNLetter/Logos/Nodejs'
-import JavaScript from './MERNLetter/Logos/JavaScript'
-import TailwindCSS from './MERNLetter/Logos/TailwindCSS'
-import Nextjs from './MERNLetter/Logos/Nextjs'
+import MongoDB from '../../../public/icons/MongoDB'
+import Expressjs from '../../../public/icons/Expressjs'
+import ReactJS from '../../../public/icons/ReactJS'
+import Nodejs from '../../../public/icons/Nodejs'
+import JavaScript from '../../../public/icons/JavaScript'
+import TailwindCSS from '../../../public/icons/TailwindCSS'
+import Nextjs from '../../../public/icons/Nextjs'
 import GitHub from '../ContactLinks/SVG/GitHub'
-import PostgreSQL from './MERNLetter/Logos/PostgreSQL'
-import MySQL from './MERNLetter/Logos/MySQL'
-import HTML from './MERNLetter/Logos/HTML'
-import CSS from './MERNLetter/Logos/CSS'
-import Miro from './MERNLetter/Logos/Miro'
+import PostgreSQL from '../../../public/icons/PostgreSQL'
+import MySQL from '../../../public/icons/MySQL'
+import HTML from '../../../public/icons/HTML'
+import CSS from '../../../public/icons/CSS'
+import Miro from '../../../public/icons/Miro'
 
 export default function Stack() {
     return (
-        <div className='w-full min-h-[100dvh] flex justify-around items-start px-28'>
-            <div className='m-8 flex-1'>
-                <h2 className='text-5xl mb-4'>But what's <span className='text-yellow-400'>MERN</span>?</h2>
-                <p className='text-2xl text-slate-400 w-1/2'>Mern is a set of tools that allow us to build complete application from top to bottom without ever leaving JavaScript.</p>
+        <div className='w-full min-h-[100dvh] flex justify-center items-start px-28'>
+            <div className='m-8 w-[60%]'>
+                <h2 className='text-5xl'>But what's <span className='text-yellow-400'>MERN</span>?</h2>
+                <p className='text-2xl text-slate-400 w-[80%] my-5'>MERN is the set of tools of my choice that allow me to build complete application from top to bottom without ever leaving JavaScript.</p>
                 <MERNLetter name='MongoDB' delay={0} icon={<MongoDB />} color='green' subtitle='NoSQL Database' />
                 <MERNLetter name='Express' delay={10} icon={<Expressjs />} color='yellow' subtitle='Back end Framework to build RESTful APIs' />
                 <MERNLetter name='React' delay={10} color='cyan' icon={<ReactJS />} subtitle='Front end Framework to build user interfaces' />
@@ -27,7 +27,7 @@ export default function Stack() {
                 <div className='bg-cyan-400 text-cyan-400 bg-lime-400 text-lime-400 bg-green-400 text-green-400'></div>
             </div>
             <div className='m-8'>
-                <h3 className='text-3xl mb-4'>Other <span>Technologies</span></h3>
+                <h3 className='text-3xl mb-8'>Other <span className='text-yellow-400'>Technologies</span> I use</h3>
                 <ul  className='grid grid-cols-3'>
                     <li className='font-bold h-32 w-32 text-sm flex flex-col justify-around items-center p-4 bg-yellow-400 text-black '><JavaScript/> JavaScript</li>
                     <li className='font-bold h-32 w-32 text-sm flex flex-col justify-around items-center p-4 bg-cyan-700 text-white'><TailwindCSS/> Tailwind</li>
