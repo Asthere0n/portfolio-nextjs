f201310783ad6fd5391ed9476b16c6934b6694d7
Cristian Sanchez <cristiansanchezsonido@gmail.com>
2025-06-17 10:15:52 +0100

    Created flipping WorkCards for all my working experiences.

--- src/components/Work/Work.tsx
+++ src/components/Work/Work.tsx
@@ -2,32 +2,90 @@ import Image from "next/image";
 import React from "react";
 import { Timeline } from "../ui/timeline";
 import WorkCard from "./WorkCard/WorkCard";
+import { title } from "process";
+import { Content } from "next/font/google";
 
 export function Work() {
-    const data = [
-  {
+  const data = [
+    {
       title: "2025",
       content: (
-          <div className="flex flex-col">
-            <WorkCard title="Barista Maestro" company="Costa Coffee" dates="Feb-2025 ~ Jun-2025" location="Stirling, UK" description="Making coffee" src="/img/costa-coffee.jpg" />
+        <div className="flex flex-col">
+          <WorkCard
+            title="Supervisor"
+            company="Costa Coffee"
+            dates="Feb-2025 ~ Jun-2025"
+            location="Stirling, UK"
+            description="At Costa Coffee I led daily operations while applying my growing web development skills to real business problems. I built tools like the Task Manager and Day-Dot Maker to improve team efficiency and reduce errors. This role sharpened both my leadership and my ability to deliver practical software solutions in real work environments."
+            src="/img/costa-coffee.jpg"
+            responsabilities={["Designed custom web tools to improve store processes", "Supervised daily store operations and team performance", "Conducted inventory checks and managed stock orders", "Delivered customer service and handled conflict resolution"]}
+          />
         </div>
       ),
     },
     {
       title: "2024",
       content: (
-          <div>
-              <p>Work experience in 2025</p>
+        <div>
+          <WorkCard
+            title="Sales Assistant"
+            src="/img/ellies.jpeg"
+            company="Ellie's cellar"
+            dates="April-2024 ~ Feb-2025"
+            location="Alva, UK"
+            responsabilities={["Managed daily operations independently while working solo shifts","Handled inventory checks and organized deliveries for multiple branches","Completed end-of-day banking and shop closing procedures","Provided personalized customer service and product recommendations"]}
+            description="At Ellie’s Cellar I balanced a part-time customer service while starting my journey into full-stack development. I worked independently, assisting customers, managing deliveries, handling banking, and overseeing daily operations alone."
+          />
         </div>
       ),
     },
     {
-      title: "2023",
+      title: "2021",
       content: (
-          <div>
-              <p>Work experience in 2025</p>
+        <div>
+          <WorkCard
+            title="Senior Bar Manager (SWAT)"
+            src="/img/joe.png"
+            location="London, UK"
+            company="Joe & the Juice"
+            dates="Sept-2021 ~ Feb-2024"
+            responsabilities={["Became Store Manager in 3 months","Managed teams of 40+ staff with focus on training and performance","Selected for SWAT Team handling store openings & recovery projects","Collaborated with Head office and area managers on high-priority assignments"]}
+            description="At Joe & The Juice I advanced quickly from team Member to store manager in 3 months, leading a 40-person team at Victoria. After two years I joined the SWAT Team, handling store launches, manager training, and turnaround projects across multiple locations. This role sharpened my leadership, training, problem-solving, and cross-team collaboration — skills directly transferable to complex software projects."
+          />
         </div>
       ),
+    },
+    {
+      title: "2018",
+      content: (
+        <div>
+          <WorkCard
+            title="Barista"
+            src="/img/pret.png" 
+            location="London, UK"
+            company="Pret A Manger"
+            dates="April-2018 ~ March-2021"
+            responsabilities={["Built and maintained a strong local network with nearby store teams","Optimized workflow and procedures in high-volume high-preassure environments","Operated under high-pressure conditions in one of the UK’s busiest shops"]}
+            description="At Pret A Manger I managed daily barista duties while also coordinating supply logistics between shops around King’s Cross. I built a strong network with nearby teams to ensure smooth operations. Working in one of the UK’s busiest locations taught me to optimize workflows, stay calm under pressure, and adapt quickly."
+          />
+        </div>
+      ),
+    },
+    {
+      title: "2017",
+      content: (
+        <div className="flex flex-col">
+          <WorkCard
+            title="Electronic Maintenance Technician"
+            src="/img/Lerche.jpg"
+            location="Valencia, Spain"
+            company="Grupo Lerche"
+            dates="Sept-2017 ~ April-2018"
+            responsabilities={["Conducted quality control inspections on LED display panels", "Performed diagnostic testing to identify hardware defects", "Documented technical reports for repair teams"]}
+            description="At Grupo Lerche I installed and maintained LED advertising screens for football stadiums, including Valencia CF and Real Madrid. I specialized in quality control, using diagnostic tests to detect hardware defects, documenting issues, and ensuring all units met strict quality standards before shipment. This role strengthened my attention to detail, problem-solving, and technical troubleshooting — skills I now apply in software development."
+          />
+        </div>
+      )
     },
   ];
   return (
--- src/components/Work/WorkCard/WorkCard.tsx
+++ src/components/Work/WorkCard/WorkCard.tsx
@@ -9,6 +9,7 @@ export interface Props {
     location: string,
     description: string,
     src: string,
+    responsabilities: string[]
 }
 
 export default function WorkCard(props: Props) {
@@ -17,18 +18,25 @@ export default function WorkCard(props: Props) {
             <div className={styles["flip-card"]}>
                 <div className={styles["flip-card-inner"]}>
                     <div className={styles["flip-card-front"]}>
-                        <div className='flex rounded-2xl border-2 border-white overflow-hidden'>
-                            <Image src={props.src} alt='' width={300} height={300} />
-                            <div className='p-4 flex flex-col justify-center items-start gap-2'>
+                        <div className='w-full h-full grid grid-cols-3 grid-rows-3 p-4 rounded-2xl border-2 justify-around items-start border-white overflow-hidden'>
+                            <div className=' flex justify-start items-center gap-4 col-start-1 col-end-4' >
+                                <Image src={props.src} alt='' width={60} height={60} className='rounded-sm'/>
                                 <p className='text-2xl font-bold'>{props.title}</p>
+                            </div>
+                            <div className='row-start-2 row-end-4 flex flex-col justify-center items-start h-full'>
                                 <p>{props.location}</p>
                                 <p>{props.dates}</p>
                             </div>
+                            <ul className='row-start-2 row-end-4 col-start-2 col-end-4 flex flex-col h-full gap-1 px-8 border-l-2 border-slate-600 justify-center items-start'>
+                                {props.responsabilities.map((resp, idx) => (
+                                    <li key={"id"+idx} className='list-disc text-left'>{resp}</li>
+                                ))}
+                            </ul>
                         </div>
                     </div>
                     <div className={styles["flip-card-back"]}>
-                        <div className='w-full h-full flex rounded-2xl border-2 border-white overflow-hidden p-4'>
-                            <p>{props.description}</p>
+                        <div className='w-full h-full flex rounded-2xl border-2 border-white overflow-hidden p-4 px-8'>
+                            <p className='text-left flex h-full items-center'>{props.description}</p>
                         </div>
                     </div>
                 </div>
